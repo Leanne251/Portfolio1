ec6dec3662b22fc93142ea8fee312aaf88a05e2c
Leanne251 <leannesmith251@gmail.com>
2022-02-27 11:02:13 +0000

    changed A to small case on Anton font import

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ChakraProvider, CSSReset, Box } from '@chakra-ui/react';
 import theme from '../styles/theme';
 import '../styles/index.css';
-import '@fontsource/Anton/400.css';
+import '@fontsource/anton/400.css';
 import '@fontsource/raleway/400.css';
 import '@fontsource/open-sans/700.css';
 import NavBar from './components/NavBar/NavBar';

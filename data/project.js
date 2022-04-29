2730475e0d405dce510a65eac6ff16508d859d53
Leanne251 <leannesmith251@gmail.com>
2022-04-29 17:28:35 +0100

    adding projects

---
+++ data/project.js
@@ -0,0 +1,48 @@
+export const myWork = [
+	{
+		id: 1,
+		title: 'Vanilla JavaScript',
+		header: 'A simple recipe search app',
+		link: 'https://vanilla-js-recipe-search.netlify.app',
+		info:
+			'This recipe app was built over Christmas 2021 after 6 weeks with School of Code Bootcamp. I used vanilla javascript, vanilla CSS and HTML to create a simple recipe search application. The data is collected from an online API called Spoonacular. My biggest challange was collecting a specific recipe ID from one end point to be used in another API call.'
+	},
+	{
+		id: 2,
+		title: 'Recipes With React',
+		header: 'Redesigning recipes with React.js',
+		link: 'https://react-js-any-ingredients.netlify.app/',
+		info:
+			'After Christmas we starting working with React.js so I redesigned my simple recipe app using my new skills. I worked in an agile manor where my first sprint searched for just one ingredient. I then progressed to search for three ingredients like the original project and my final sprint now enables the user to choose the number of ingredients they would like to include & how many recipes they would like to choose from. Lastly the project was styled using Chakra UI. '
+	},
+	{
+		id: 3,
+		title: 'Bootcamper Feedback App',
+		header: 'Working with a group of four',
+		link: 'https://bootcampers-social.netlify.app/',
+		info:
+			'Week 9 of School of Code introduced me to my first full week of engineering. Our team developed a full stack application which would be helpful for new bootcampers. As bootcampers work remotely we decided to build a platform where they could annoymously share their thoughts & feelings of the day and give each other encouragement on such a fast-paced course. With just a week, the project was quite intense and we hit a few stumbling blocks when it came to deploying - however, we hit these early & remained calm to produce our two page application. '
+	},
+	{
+		id: 4,
+		title: 'Life Lifter App',
+		header: 'Working in a team of six',
+		link: 'https://lifelifter.netlify.app/',
+		info:
+			'The final part of the School of Code bootcamp includes a 4 week project. We had a blank slate when deciding our problem so we used a Miro board to brain storm different ideas. There was a lot of discussin about fitness, health & wellbeing so we decided to combine these topics into a personalised goal-setting app. The project enabled you to select different exercises, recipes, and set personal goals which could be diarised to show up on your dashboard each day. This enabled the user to log in and tick them off as they completed them. To develop our app we used an external API for data as well as creating our own database to store the users schedule and making fetch requests on the required date. We also used Firebase authentication to personalise the application to the user. '
+	},
+	{
+		id: 5,
+		title: 'Holiday Project',
+		header: 'Implementing skills in front & back end',
+		link: '#',
+		info: `I've started working on a full stack application which integrates Firebase for authorisation and uses Cloudinary to allow a user to upload their own pictures from their computer. The user can view all holidays submitted by everyone, submit their own holidays and save 'bucket list' holidays to their account. This project is a work-in-progress and is built using React in the front-end and Node.js & Express in the back-end. Data is stored using Cloudinary & Heroku Postgres SQL database.`
+	},
+	{
+		id: 6,
+		title: "What's Next? ",
+		header: 'Future Challenges',
+		link: '#',
+		info: `My next challange is to learn the Angular framwork, PHP for the back end and ElasticSearch. These skills will help me grow in first developer role with VoxPopMe`
+	}
+];
